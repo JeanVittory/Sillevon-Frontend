53ad0e66a363b8cc1d48c37499a41f57ae5fb169
JeanVittory <vittory.dev@gmail.com>
2023-05-16 20:29:39 -0500

    next 13 migration stable

---
+++ src/components/MusicianSmallCard.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useMusicianSmallCardStyles } from './ui/useMusicianSmallCardStyles';
+import { UnstyledButton, UnstyledButtonProps, Group, Avatar, Text } from '@mantine/core';
+import { IconChevronRight } from '@tabler/icons-react';
+
+interface MusicianSmallCardProps extends UnstyledButtonProps {
+	image: string;
+	name: string;
+	email: string;
+	icon?: React.ReactNode;
+}
+
+export function MusicianSmallCard({ image, name, email, icon, ...others }: MusicianSmallCardProps) {
+	const { classes } = useMusicianSmallCardStyles();
+
+	return (
+		<UnstyledButton className={classes.user} {...others}>
+			<Group>
+				<Avatar src={image} radius='xl' />
+
+				<div style={{ flex: 1 }}>
+					<Text size='sm' weight={500}>
+						{name}
+					</Text>
+
+					<Text color='dimmed' size='xs'>
+						{email}
+					</Text>
+				</div>
+
+				{icon || <IconChevronRight size={14} stroke={1.5} />}
+			</Group>
+		</UnstyledButton>
+	);
+}
---
+++ src/components/User.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { UnstyledButton, UnstyledButtonProps, Group, Avatar, Text } from '@mantine/core';
+import { useUserStyles } from './ui/useUserStyles';
+import { IconChevronRight } from '@tabler/icons-react';
+import { Dispatch, SetStateAction } from 'react';
+import { ChatProps } from '../app/profile/client/chat';
+
+interface UserButtonProps extends UnstyledButtonProps {
+	index: number;
+	setIsSelected: Dispatch<SetStateAction<number | null>>;
+	setCurrentChat: Dispatch<SetStateAction<ChatProps | null>>;
+	user: any;
+}
+
+export function User({ index, setCurrentChat, user, setIsSelected }: UserButtonProps) {
+	const { classes } = useUserStyles();
+
+	return (
+		<UnstyledButton
+			className={classes.user}
+			onClick={() => {
+				setCurrentChat(user);
+				setIsSelected(index + 1);
+			}}
+		>
+			<Group>
+				<Avatar src={user.imagesDone.avatar} radius='xl' />
+				<div style={{ flex: 1 }}>
+					<Text size='sm' weight={500}>
+						{user.name}
+					</Text>
+					<Text color='dimmed' size='xs'>
+						{user.instrument}
+					</Text>
+				</div>
+				<IconChevronRight size={14} stroke={1.5} />
+			</Group>
+		</UnstyledButton>
+	);
+}
